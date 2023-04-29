d2e6252ad5e46c781716ef3c83c95955bb27f7ac
Axis254 <56816349+Axis254@users.noreply.github.com>
2023-04-29 19:46:56 +0300

    Add files via upload

---
+++ Emergency Casualty/assets/inc/admin.php
@@ -0,0 +1,47 @@
+<?php include_once 'assets/inc/_admin_nav.php'; ?>
+
+<div class="container-fluid">
+    <div class="row">
+        
+        <div class="col-md-6">
+            <div class="card bg-dark text-white">
+                
+                <div class="card-img-overlay">
+                    <p class="card-text text-info"></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card border-danger">
+                    <img class="card-img-top" src="assets/images/slide5.jfif" alt="">
+                    <div class="card-body">
+                        <a href="emergency.php" class="btn btn-outline-danger btn-block">Emergencies</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card border-success">
+                    <img class="card-img-top" src="assets/images/slide4.jfif" alt="">
+                    <div class="card-body">
+                        <a href="dispatch.php" class="btn btn-outline-success btn-block">Dispatchers</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card border-info">
+                    <img class="card-img-top" src="assets/images/report2.jfif" alt="">
+                    <div class="card-body">
+                        <a href="#" class="btn btn-outline-info btn-block">Reports</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+</div>
